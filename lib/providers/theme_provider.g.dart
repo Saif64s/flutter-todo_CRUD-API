88f62d338e1ec19f2dcaced22d68bd983489e04f
saif64s <my@wo.rk>
2023-08-08 11:23:10 +0600

    refactoring on macOS

---
+++ lib/providers/theme_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$darkThemeHash() => r'9db116aaf28c86dce50df21d6150e56d7f7e208e';
+
+/// See also [DarkTheme].
+@ProviderFor(DarkTheme)
+final darkThemeProvider =
+    AutoDisposeNotifierProvider<DarkTheme, ThemeMode>.internal(
+  DarkTheme.new,
+  name: r'darkThemeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$darkThemeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DarkTheme = AutoDisposeNotifier<ThemeMode>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member
